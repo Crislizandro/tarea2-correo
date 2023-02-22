280552633e6e1cecde78cb03da562f781fb07152
user <user@DESKTOP-GSD4I6C>
2023-02-21 19:59:49 -0600

    Add project files.

---
+++ tarea2 correo/Page1principal.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tarea2_correo.model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tarea2_correo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1principal : ContentPage
+    {
+        public Page1principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btENVIAR_Clicked(object sender, EventArgs e)
+        {
+
+            var emple = new Empleado
+            {
+                nombre = txtnumero1.Text,
+                apellidos=txtnumero2.Text,
+                edad=txtnumero3.Text,
+                correo=txtnumero4.Text,
+            
+            };
+
+            var secondpage = new PageResultado();
+            secondpage.BindingContext= emple;
+            Navigation.PushAsync (secondpage);
+        }
+    }
+}
---
+++ tarea2 correo/model/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tarea2_correo.model
+{
+    internal class Empleado
+    {
+
+        public string nombre { get; set; }
+        public string apellidos { get; set;}
+        public string edad { get;set; }
+        public string correo { get; set; }
+
+    }
+}
